f71d0430332bd2883c8e6008668d44817fd8f46b
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2022-07-06 20:59:11 +0100

    basic 3d convolutional autoencoder model

---
+++ fr3D/models/ConvAutoencoder.py
@@ -0,0 +1,209 @@
+import tensorflow as tf
+import os
+
+conv_layers = {1: tf.keras.layers.Conv1D,
+               2:tf.keras.layers.Conv2D,
+               3:tf.keras.layers.Conv3D}
+deconv_layers = {1: tf.keras.layers.Conv1DTranspose,
+                 2:tf.keras.layers.Conv2DTranspose,
+                 3:tf.keras.layers.Conv3DTranspose}
+pool_layers = {1: tf.keras.layers.AveragePooling1D,
+               2: tf.keras.layers.AveragePooling2D,
+               3: tf.keras.layers.AveragePooling3D}
+normalization_layers = {'layernorm': tf.keras.layers.LayerNormalization,
+                        'batchnorm': tf.keras.layers.BatchNormalization}
+
+def conv_block(input_tensor, filters: int, kernel_size: int = 3, ndims: int = 3, activation = None, normalization = None, pre_activation: bool = False, residual_connection: bool = False):
+
+    x = input_tensor
+
+    for k in range(2):
+        if normalization is not None:
+            norm_axis = 1 if tf.keras.backend.image_data_format()=='channels_first' else -1
+            x = normalization_layers[normalization](axis=norm_axis)(x)
+        if pre_activation:
+            x = tf.keras.layers.Activation(tf.nn.relu)(x)
+        x = conv_layers[ndims](filters, kernel_size, padding='same', activation=activation)(x)
+
+    if residual_connection:
+        x = x + input_tensor
+        
+    return x
+
+def encoder_block(input_tensor, levels: int, base_filters: int, kernel_size: int = 3, activation = None, normalization = None, pre_activation: bool = True, residual_connection: bool = True, blocks_per_level: int = 4, pool_size: int = 2):
+
+    ndims = len(input_tensor.shape)-2
+
+    x = input_tensor
+
+    x = conv_layers[ndims](base_filters, 7, padding='same', activation=activation)(x)
+    x = pool_layers[ndims](padding='same', pool_size = pool_size)(x)
+
+    for k in range(levels):
+        level_filters = (2**(k+1))*base_filters
+        x = conv_layers[ndims](level_filters, kernel_size, activation=activation, padding='same')(x)
+        for _ in range(blocks_per_level):
+            x = conv_block(x,
+                           filters=level_filters,
+                           kernel_size=kernel_size,
+                           ndims=ndims,
+                           activation=activation,
+                           normalization=normalization,
+                           pre_activation=pre_activation,
+                           residual_connection=residual_connection)
+
+        if k < (levels-1):
+            x = pool_layers[ndims](padding='same', pool_size = pool_size)(x)
+
+    return x
+
+def decoder_block(input_tensor, levels: int, output_filters: int, kernel_size: int = 3, activation = None, final_activation = None, pre_activation: bool = True, residual_connection: bool = True, normalization = None, blocks_per_level: int = 4, deconv_stride: int = 2):
+
+    norm_axis = 1 if tf.keras.backend.image_data_format()=='channels_first' else -1
+    base_filters = input_tensor.shape[norm_axis]
+    ndims = len(input_tensor.shape)-2
+    
+    x = input_tensor
+
+    for _ in range(blocks_per_level):
+        x = conv_block(x,
+                       filters=base_filters,
+                       kernel_size=kernel_size,
+                       ndims=ndims,
+                       activation=activation,
+                       normalization=normalization,
+                       pre_activation=pre_activation,
+                       residual_connection=residual_connection)
+
+    for k in range(levels):
+        level_filters = base_filters//(2**(k+1))
+        x = conv_layers[ndims](level_filters, kernel_size, activation=activation, padding='same')(x)
+        x = deconv_layers[ndims](level_filters, kernel_size, strides=deconv_stride, activation=activation, padding='same')(x)
+        for _ in range(blocks_per_level):
+            x = conv_block(x,
+                           filters=level_filters,
+                           kernel_size=kernel_size,
+                           ndims=ndims,
+                           activation=activation,
+                           normalization=normalization,
+                           pre_activation=pre_activation,
+                           residual_connection=residual_connection)
+
+    x = conv_layers[ndims](output_filters, kernel_size, activation=final_activation, padding='same')(x)
+            
+    return x
+
+
+class ConvAutoencoder(tf.keras.models.Model):
+    def __init__(self, levels: int, base_filters: int,
+                 kernel_size: int = 3,
+                 activation = None,
+                 normalization: str = None,
+                 pre_activation: bool = True,
+                 residual_connection: bool = True,
+                 blocks_per_level: int = 4,
+                 pool_size: int = 2,
+                 input_shape = None, ndims: int = 3, filters: int = None):
+
+        super().__init__()
+
+        if activation == 'leaky_relu':
+            activation = tf.nn.leaky_relu
+        
+        if input_shape is None and filters is None:
+            raise(ValueError('Either input_shape or (filters and ndims) must be provided.'))
+        elif input_shape is None:
+            if tf.keras.backend.image_data_format() == 'channels_last':
+                input_shape = [None for _ in range(ndims)] + [filters]
+            else:
+                input_shape = [filters] + [None for _ in range(ndims)]
+        
+        inp = tf.keras.layers.Input(input_shape)
+        channel_axis = -1 if tf.keras.backend.image_data_format() == 'channels_last' else 1
+        output_filters = inp.shape[channel_axis]
+
+        encoder_out = self.create_encoder(input_tensor=inp,
+                                          levels=levels,
+                                          base_filters=base_filters,
+                                          kernel_size=kernel_size,
+                                          activation=activation,
+                                          normalization=normalization,
+                                          pre_activation=pre_activation,
+                                          residual_connection=residual_connection,
+                                          blocks_per_level=blocks_per_level,
+                                          pool_size=pool_size)
+
+        self.encoder = tf.keras.Model(inp, encoder_out, name='encoder')
+
+        decoder_out = self.create_decoder(input_tensor=encoder_out,
+                                          output_filters=output_filters,
+                                          levels=levels,
+                                          kernel_size=kernel_size,
+                                          activation=activation,
+                                          normalization=normalization,
+                                          pre_activation=pre_activation,
+                                          residual_connection=residual_connection,
+                                          blocks_per_level=blocks_per_level,
+                                          deconv_stride=pool_size)
+
+        self.decoder = tf.keras.Model(encoder_out, decoder_out, name='decoder')
+
+        self.build(inp.shape)
+
+    @staticmethod
+    def create_encoder(*args, **kwargs):
+        return encoder_block(*args, **kwargs)
+
+    @staticmethod
+    def create_decoder(*args, **kwargs):
+        return decoder_block(*args, **kwargs)
+
+    '''
+    def save_weights(self, path, overwrite=True, save_format=None, options=None):
+        filename, file_ext = os.path.splitext(path)
+        encoder_filename = filename + '_encoder' + file_ext
+        decoder_filename = filename + '_decoder' + file_ext
+        self.encoder.save_weights(encoder_filename, overwrite=overwrite, save_format=save_format, options=options)
+        self.decoder.save_weights(decoder_filename, overwrite=overwrite, save_format=save_format, options=options)
+
+    def save(self, path, overwrite=True, include_optimizer=True, save_format=None, signatures=None, options=None, save_traces=True):
+        filename, file_ext = os.path.splitext(path)
+        encoder_filename = filename + '_encoder' + file_ext
+        decoder_filename = filename + '_decoder' + file_ext
+        self.encoder.save(encoder_filename, overwrite=overwrite, save_format=save_format, options=options, include_optimizer=include_optimizer, save_traces=save_traces, signatures=signatures)
+        self.decoder.save(decoder_filename, overwrite=overwrite, save_format=save_format, options=options, include_optimizer=include_optimizer, save_traces=save_traces, signatures=signatures)
+
+    def load_weights(self, encoder_path, decoder_path):
+        self.encoder.load_weights(encoder_path)
+        self.decoder.load_weights(decoder_path)
+    '''
+    
+    def call(self, inp):
+        return self.decoder(self.encoder(inp))
+                     
+    def train_step(self, x):
+        return super().train_step((x,x))
+
+    def test_step(self, x):
+        return super().test_step((x,x))
+        
+
+if __name__ == '__main__':
+
+    mod = ConvAutoencoder(levels=4,
+                          base_filters=32,
+                          normalization='batchnorm',
+                          activation=tf.nn.leaky_relu,
+                          pre_activation=True,
+                          residual_connection=True,
+                          input_shape=[64,64,64,3]
+                          )
+
+    mod.save_weights('/tmp/test.h5')
+
+    
+    import pdb; pdb.set_trace()
+
+    
+    
+    
---
+++ fr3D/train/ConvAutoencoder.py
@@ -0,0 +1,66 @@
+import argparse
+import json
+import tensorflow as tf
+import numpy as np
+
+from .utils import modify_node_types, prepare_dataset_for_training
+from ..models.ConvAutoencoder import ConvAutoencoder
+from ..data import DatasetPipelineBuilder
+from ..data.utils import hdf5_train_test_split
+
+tf.keras.backend.set_image_data_format('channels_last')
+#tf.keras.mixed_precision.set_global_policy('mixed_float16')
+
+#parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('experiment_config', type=str)
+parser.add_argument('dataset_path', type=str)
+parser.add_argument('checkpoint_path', type=str)
+parser.add_argument('--load_weights', type=str, default=None)
+parser.add_argument('--shuffle_size', type=int, default=500)
+args = parser.parse_args()
+
+config = json.load(open(args.experiment_config,'r'))
+
+#setup datasets
+node_configs = modify_node_types(config['dataset']['node_configurations'], 'HDF5IODataset', 'filepath', args.dataset_path)
+
+if 'train_test_split' in config['dataset']:
+    np.random.seed(42)
+    train_geometries, test_geometries = hdf5_train_test_split(args.dataset_path, config['dataset']['train_test_split'][:2], shuffle=True)
+    print(f'Training geometries: {train_geometries}')
+    print(f'Test geometries: {test_geometries}')
+    train_node_configurations = modify_node_types(node_configs, 'HDF5IODataset', 'groups', train_geometries)
+    test_node_configurations = modify_node_types(node_configs, 'HDF5IODataset', 'groups', test_geometries)
+
+    test_dataset_pipeline = DatasetPipelineBuilder(test_node_configurations)
+    test_dataset = prepare_dataset_for_training(test_dataset_pipeline.get_node(config['dataset']['training_node']).dataset, config['dataset']['batch_size'], args.shuffle_size)
+else:
+    train_node_configurations = node_configs
+    test_dataset = None
+
+train_dataset_pipeline = DatasetPipelineBuilder(train_node_configurations)
+train_dataset = prepare_dataset_for_training(train_dataset_pipeline.get_node(config['dataset']['training_node']).dataset, config['dataset']['batch_size'], args.shuffle_size)
+
+dummy_input = next(iter(train_dataset))
+input_shape = dummy_input.shape[1:]
+del dummy_input
+
+#create model and train
+callbacks = [tf.keras.callbacks.ModelCheckpoint(args.checkpoint_path, save_best_only=True, save_weights_only=True)]
+if 'reduce_lr' in config['training']:
+    callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(**config['training']['reduce_lr']))
+if 'early_stopping' in config['training']:
+    callbacks.append(tf.keras.callbacks.EarlyStopping(**config['training']['early_stopping']))
+
+distribute_strategy =  tf.distribute.MirroredStrategy()
+with distribute_strategy.scope():
+    model = ConvAutoencoder(input_shape=input_shape, **config['model'])
+    model.summary()
+    loss_fn = tf.keras.losses.get(config['training']['loss'])
+    model.compile(loss=loss_fn, optimizer = tf.keras.optimizers.get(config['training']['optimizer']), metrics = config['training'].get('metrics', None))
+
+    if args.load_weights is not None:
+        model.load_weights(args.load_weights)
+    
+    model.fit(train_dataset, epochs = config['training']['epochs'], callbacks = callbacks, validation_data = test_dataset, validation_steps = config['training'].get('validation_steps', None))
