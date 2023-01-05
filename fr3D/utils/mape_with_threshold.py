e2a2e9ddc5f1e922346f1d18afb54c07eddaa083
Ali Girayhan Ozbay <ago14@kolmogorov.ae.ic.ac.uk>
2023-01-05 23:38:41 +0000

    mape with threshold updates

--- fr3D/utils/mape_with_threshold.py
+++ fr3D/utils/mape_with_threshold.py
@@ -2,10 +2,15 @@ import tensorflow as tf
 import numpy as np
 
 @tf.function
-def mape_with_threshold(yp, yt, pcterror_threshold=np.inf, max_magnitude_threshold=0.0, eps=1e-7):
+def mape_with_threshold(yp, yt, pcterror_threshold=np.inf, max_magnitude_threshold=0.0, eps=1e-7, sample_weights=None, axis=None):
+    if sample_weights is None:
+        sample_weights = 1.0
+        
     pct_errors = 100*tf.abs((yp-yt)/(eps + yt))
     pcterror_mask = pct_errors < pcterror_threshold
     max_magnitude_mask = tf.logical_not(tf.abs(yt) < (max_magnitude_threshold*tf.reduce_max(tf.abs(yt))))
-    filtering_indices = tf.where(tf.logical_and(pcterror_mask, max_magnitude_mask))
-    filtered_pcterrors = tf.gather_nd(pct_errors, filtering_indices)
-    return tf.reduce_mean(filtered_pcterrors)
+    cond_mask = tf.logical_and(pcterror_mask, max_magnitude_mask)
+    
+    sample_weights = sample_weights * tf.cast(cond_mask, pct_errors.dtype)
+    
+    return tf.reduce_sum(sample_weights*pct_errors, axis=axis)/tf.reduce_sum(sample_weights, axis=axis)
